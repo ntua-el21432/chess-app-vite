74f657e03cd37b0ef09a2b0eb8163660411ad25c
billkotronis <el21432@mail.ntua.gr>
2025-02-18 02:15:53 +0200

    please

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port=process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const clientPath=path.join(__dirname,"client","dist");
+const clientPath=path.join(__dirname,"../client","dist");
 
 //setup middleware
 app.use(express.static(clientPath));
